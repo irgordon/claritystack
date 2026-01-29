6cebc0aa8715c8b07e5b41a9538aa91339bcbb5b
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-29 18:01:04 -0500

    Create Security.php

---
+++ api/core/Security.php
@@ -0,0 +1,17 @@
+<?php
+namespace Core;
+class Security {
+    // In production, load this from an environment variable!
+    private static $key = 'YOUR_APP_KEY_CHANGE_THIS_IN_PROD'; 
+
+    public static function encrypt($data) {
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+        $encrypted = openssl_encrypt($data, 'aes-256-cbc', self::$key, 0, $iv);
+        return base64_encode($encrypted . '::' . $iv);
+    }
+
+    public static function decrypt($data) {
+        list($encrypted_data, $iv) = explode('::', base64_decode($data), 2);
+        return openssl_decrypt($encrypted_data, 'aes-256-cbc', self::$key, 0, $iv);
+    }
+}
