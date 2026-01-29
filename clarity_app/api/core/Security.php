df3e1bf557102f7d91644952a0a2a6aee69273f5
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-29 18:14:37 -0500

    Update and rename api/core/Security.php to clarity_app/api/core/Security.php

--- clarity_app/api/core/Security.php
+++ clarity_app/api/core/Security.php
@@ -1,8 +1,11 @@
 <?php
 namespace Core;
+
 class Security {
-    // In production, load this from an environment variable!
-    private static $key = 'YOUR_APP_KEY_CHANGE_THIS_IN_PROD'; 
+    // In production, load this from .env
+    // key example 'b1a2c3d4e5f60718293a4b5c6d7e8f90123456789abcdef0fedcba9876543210' do not use this one obviously
+    //
+    private static $key = 'CHANGE_THIS_TO_A_LONG_RANDOM_STRING_IN_PROD'; 
 
     public static function encrypt($data) {
         $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
@@ -11,7 +14,9 @@ class Security {
     }
 
     public static function decrypt($data) {
+        if (!$data) return '';
         list($encrypted_data, $iv) = explode('::', base64_decode($data), 2);
         return openssl_decrypt($encrypted_data, 'aes-256-cbc', self::$key, 0, $iv);
     }
 }
+?>
