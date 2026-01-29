22b1c4bb7c028a4b83cfcf6b17a5a3ee3836aefb
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-29 18:42:13 -0500

    Create view.php

---
+++ clarity_app/themes/clarity_default/blocks/text_simple/view.php
@@ -0,0 +1,9 @@
+<?php
+$content = $content ?? '<p>Add your text here...</p>';
+?>
+
+<div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+    <div class="prose prose-lg prose-blue mx-auto text-gray-500">
+        <?= $content ?>
+    </div>
+</div>
