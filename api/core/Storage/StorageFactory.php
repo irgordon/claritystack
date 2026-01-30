6e7e493548961ec03c7a505e30019eb525500e59
Ian <118707480+irgordon@users.noreply.github.com>
2026-01-29 19:10:55 -0500

    Create StorageFactory.php

---
+++ api/core/Storage/StorageFactory.php
@@ -0,0 +1,38 @@
+<?php
+namespace Core\Storage;
+
+class StorageFactory {
+    public static function create(array $config): StorageInterface {
+        $driver = $config['STORAGE_DRIVER'] ?? 'local';
+
+        switch ($driver) {
+            case 'cloudinary':
+                return new CloudinaryAdapter([
+                    'cloud_name' => $config['CLOUDINARY_NAME'],
+                    'api_key'    => $config['CLOUDINARY_KEY'],
+                    'api_secret' => $config['CLOUDINARY_SECRET'],
+                ]);
+            
+            case 'imagekit':
+                return new ImagekitAdapter([
+                    'public_key'   => $config['IMAGEKIT_PUBLIC'],
+                    'private_key'  => $config['IMAGEKIT_PRIVATE'],
+                    'url_endpoint' => $config['IMAGEKIT_URL'],
+                ]);
+
+            case 'drive':
+                return new GoogleDriveAdapter([
+                    'service_account_json_path' => __DIR__ . '/../../config/service-account.json',
+                    'root_folder_id' => $config['DRIVE_ROOT_FOLDER'],
+                ]);
+
+            case 's3':
+                // (Existing S3 Logic)
+                return new S3Adapter($config);
+
+            case 'local':
+            default:
+                return new LocalAdapter($config['STORAGE_PATH'] ?? __DIR__ . '/../../../../storage_secure');
+        }
+    }
+}
